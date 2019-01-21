887d74a00b809fd207bc6fd159c0158988b55bfe
huangwwmm <hwm@huangwm.com>
2019-01-21 22:21:01 +0800

    add: create window & vertex

--- Software Renderer/main.cpp
+++ Software Renderer/main.cpp
@@ -3,21 +3,94 @@
 #include <iostream>
 #include <intrin.h>
 #include <math.h>
+#include <windows.h>
+#include <windowsx.h>
+#include <mmsystem.h>
 
-
-int main()
+LRESULT CALLBACK WindowProc(HWND hwnd
+	, UINT message
+	, WPARAM wparam
+	, LPARAM lparam)
 {
-	//FVector v(5.0f, 8.0f, 7.0f);
-	//FVector v2(5.0f, 8.0f, 7.0f);
-	////FVector v2 = v + v;
-	//bool b = v != v2;
-	//std::cout << b;
+	PAINTSTRUCT ps;
+	HDC hdc;
 
-	////FMatrix matrix2(new float[16]{ 0.0f,1.0f,2.0f,3.0f,4.0f,5.0f, 6.0f,7.0f,8.0f,9.0f, 10.f,11.0f,12.0f,13.0f,14.0f,15.0f });
-	////FMatrix matrix2(new float[16]{ 0.0f,1.0f,2.0f,3.0f,4.0f,5.0f, 6.0f,7.0f,8.0f,9.0f, 10.f,11.0f,12.0f,13.0f,14.0f,15.0f });
-	////FMatrix matrix2(new float[16]{ 0.0f,1.0f,2.0f,3.0f,4.0f,5.0f, 6.0f,7.0f,8.0f,9.0f, 10.f,11.0f,12.0f,13.0f,14.0f,15.0f });
-	//FMatrix matrix(new float[16]{ 0.0f,1.0f,2.0f,3.0f,2.0f,5.0f, 6.0f,7.0f,8.0f,9.0f, 10.f,11.0f,12.0f,13.0f,14.0f,15.0f });
-	//FMatrix matrix2(new float[16]{ 0.0f,2.0f,2.0f,3.0f,2.0f,5.0f, 6.0f,7.0f,8.0f,9.0f, 10.f,11.0f,12.0f,13.0f,14.0f,15.0f });
-	//bool b = matrix != matrix2;
-	//std::cout << b;
+	switch (message)
+	{
+	case WM_PAINT:
+		hdc = ::BeginPaint(hwnd, &ps);
+		for (int i = 0; i < 400; i++)
+		{
+			for (int j = 0; j < 50; j++)
+				SetPixel(hdc, i, j, 0x561);
+		}
+		::EndPaint(hwnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, message, wparam, lparam);
+		break;
+	}
+}
+
+int WINAPI WinMain(HINSTANCE hInstance
+	, HINSTANCE hPrevInstance
+	, LPSTR lpCmdLine
+	, int nCmdShow)
+{
+	WNDCLASS wc = { };
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = L"Sample Window Class";
+
+	RegisterClass(&wc);
+
+	// Create the window.
+
+	HWND hwnd = CreateWindowEx(
+		0,                              // Optional window styles.
+		L"Sample Window Class",         // Window class
+		L"Learn to Program Windows",    // Window text
+		WS_OVERLAPPEDWINDOW,            // Window style
+		// Size and position
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL,       // Parent window    
+		NULL,       // Menu
+		hInstance,  // Instance handle
+		NULL        // Additional application data
+	);
+
+	if (hwnd == NULL)
+	{
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+
+	MSG msg;
+
+	// Enter the infinite message loop
+	while (true)
+	{
+		// Check to see if any messages are waiting in the queue
+		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			// Translate the message and dispatch it to WindowProc()
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		// If the message is WM_QUIT, exit the while loop
+		if (msg.message == WM_QUIT)
+		{
+			break;
+		}
+
+		// UNDONE Time system
+		Sleep(33);
+	}
+
+	return (int)msg.wParam;
 }
---
+++ Software Renderer/vertex.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "vector.h"
+
+struct Vertex
+{
+	// moudel space
+	FVector position_moudel;
+	FVector normal;
+	float diffuse_uv[2];
+};
